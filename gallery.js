d45db16653ebf9b0c891be3ca721a5f85f205e07
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-08-27 14:51:21 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,66 @@
+
+ containerdiv = document.getElementById("container");
+ 
+function loadImageFileAsURL()
+{
+
+  
+
+    var filesSelected = document.getElementById("inputFileToLoad").files;
+    if (filesSelected.length > 0)
+    {
+        var fileToLoad = filesSelected[0];
+
+        if (fileToLoad.type.match("image.*"))
+        {
+            var fileReader = new FileReader();
+            fileReader.onload = function(fileLoadedEvent) 
+            {
+                var imageLoaded = document.createElement("img");
+                imageLoaded.src = fileLoadedEvent.target.result;
+                containerdiv.appendChild(imageLoaded);       
+                            };
+            fileReader.readAsDataURL(fileToLoad);
+        }
+    }
+}
+
+
+
+// Drop down punjiri screen
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }}
+
+ 
+
+
+
+
